459a97b22324ab05e6afd3f68bb3e7059cca42a3
aandrew21 <166205816+aandrew21@users.noreply.github.com>
2025-01-28 09:07:55 +0000

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,45 +1,47 @@
-CREATE DATABASE alx_book_store;
+-- Create the alx_book_store database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+-- Use the alx_book_store database
 USE alx_book_store;
 
--- Table for storing authors
+-- Create the Authors table
 CREATE TABLE Authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
+  author_id INT AUTO_INCREMENT PRIMARY KEY,
+  author_name VARCHAR(215) NOT NULL
 );
 
--- Table for storing books
+-- Create the Books table
 CREATE TABLE Books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
-    author_id INT,
-    price DOUBLE NOT NULL,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id) ON DELETE SET NULL
+  book_id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(130) NOT NULL,
+  author_id INT,
+  price DOUBLE NOT NULL,
+  publication_date DATE,
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Table for storing customers
+-- Create the Customers table
 CREATE TABLE Customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) UNIQUE NOT NULL,
-    address TEXT NOT NULL
+  customer_id INT AUTO_INCREMENT PRIMARY KEY,
+  customer_name VARCHAR(215) NOT NULL,
+  email VARCHAR(215) NOT NULL UNIQUE,
+  address TEXT
 );
 
--- Table for storing orders
+-- Create the Orders table
 CREATE TABLE Orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE
+  order_id INT AUTO_INCREMENT PRIMARY KEY,
+  customer_id INT,
+  order_date DATE NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Table for storing order details
+-- Create the Order_Details table
 CREATE TABLE Order_Details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
-    FOREIGN KEY (book_id) REFERENCES Books(book_id) ON DELETE CASCADE
+  orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
+  order_id INT,
+  book_id INT,
+  quantity DOUBLE NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
-
